ecaa7bef2c0db812e71e56947f66093f3526c708
Vinit Tomar <vinit.tomar68@gmail.com>
2022-09-17 17:21:15 +0000

    feat(todo-app): setup & create todo

--- src/components/CodeViewer/index.tsx
+++ src/components/CodeViewer/index.tsx
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 import CodeBlock from '@theme/CodeBlock';
 
-export default function CodeViewer(): JSX.Element {
+export default function CodeViewer(props: {
+  url: string,
+  language: string,
+  title: string,
+  start?: number,
+  end?: number,
+}): JSX.Element {
 
-  const [code, setCode] = useState(`function HelloCodeTitle(props) {
-      return <h1>Hello vin, {props.name}</h1>;
-    }`);
+  const {
+    url,
+    language,
+    title,
+    start,
+    end
+  } = props;
 
-  const prom = fetch('https://gist.githubusercontent.com/VinitTomar/20f73b09dfc9980ce7aa235119ebdd44/raw/af32a6368e27468d385b87dbc42dd0e0aeddfb07/Todo_with_BehaviorSubject.ts');
-  prom.then(req => req.text()).then(txt => setCode(txt));
+  const [code, setCode] = useState(`fetching...`);
 
-  return (<CodeBlock
-    language="jsx"
-    title="/src/components/HelloCodeTitle.js"
-    showLineNumbers>
-    {code}
-  </CodeBlock>
+  fetch(url)
+    .then(req => req.text())
+    .then(txt => {
+      const txtArr = txt.split('\n');
+      const startIndex = start ? start - 1 : -1;
+      const endIndex = end ? end - 1 : txtArr.length;
+      txt = txtArr.filter((_, i) => (i >= startIndex && i <= endIndex)).join('\n');
+      setCode(txt);
+    });
+
+  return (
+    <CodeBlock
+      language={language}
+      title={title}
+      showLineNumbers
+    >
+      {code}
+    </CodeBlock>
   );
 }
